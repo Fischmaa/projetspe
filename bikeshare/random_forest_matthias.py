95b547f7b13931f94adea034e27d75cdc5ce7b9c
Matthias Beaupère <matthias.beaupere@ensimag.grenoble-inp.fr>
2016-05-19 16:03:08 +0200

    random forest 0,97

--- bikeshare/random_forest_matthias.py
+++ bikeshare/random_forest_matthias.py
@@ -1,26 +1,64 @@
-#! /usr/share/python3
+#! $HOME/anaconda3/lib/python3.5
 # -*-coding:utf-8-*
 
 import pandas as pa
 from sklearn.ensemble import RandomForestClassifier as rf
 import matplotlib.pyplot as plt
-import random
-
-random.seed(1)
+from datetime import datetime
+pa.options.mode.chained_assignment = None  # default='warn'
 
 tabtrain = pa.read_csv('sources/train.csv')
 tabtest = pa.read_csv('sources/test.csv')
 
-# On forme les tableaux des features
-x_train = tabtrain.drop(['datetime','count','casual','registered'],1)
-x_test = tabtest.drop(['datetime'],1)
+tabtrain['date']=0
+for i in range(len(tabtrain)) :
+	tabtrain['date'][i]=datetime.strptime(tabtrain['datetime'][i],'%Y-%m-%d %H:%M:%S')
 
+
+
+tabtest['date']=0
+for i in range(len(tabtest)) :
+	tabtest['date'][i]=datetime.strptime(tabtest['datetime'][i],'%Y-%m-%d %H:%M:%S')
+
+# On passe les variables en discret
+tabtrain['season'] = tabtrain['season'].astype('category')
+tabtrain['holiday'] = tabtrain['holiday'].astype('category')
+tabtrain['workingday'] = tabtrain['workingday'].astype('category')
+tabtrain['weather'] = tabtrain['weather'].astype('category')
+
+tabtest['season'] = tabtest['season'].astype('category')
+tabtest['holiday'] = tabtest['holiday'].astype('category')
+tabtest['workingday'] = tabtest['workingday'].astype('category')
+tabtest['weather'] = tabtest['weather'].astype('category')
+
+
+#create day of week column and hour column then convert into category for both training and test set
+tabtrain['day']=0
+tabtrain['hour']=0
+for i in range(len(tabtrain)) :
+	tabtrain['day'][i] = tabtrain['date'][i].weekday()
+	tabtrain['hour'][i] = tabtrain['date'][i].hour
+
+tabtrain['day']=tabtrain['day'].astype('category')
+tabtrain['hour']=tabtrain['hour'].astype('category')
+
+tabtest['day']=0
+tabtest['hour']=0
+for i in range(len(tabtest)) :
+	tabtest['day'][i] = tabtest['date'][i].weekday()
+	tabtest['hour'][i] = tabtest['date'][i].hour
+
+tabtest['day']=tabtest['day'].astype('category')
+tabtest['hour']=tabtest['hour'].astype('category')
+
+# Tableaux d'entrainement
+y_train = tabtrain['count']
+x_train = tabtrain.drop(['datetime','count','casual','registered','date','day'],1)
 
 # On forme les tableaux des résultats
-y_train = tabtrain['count']
+x_test = tabtest.drop(['datetime','date','day'],1)
 
-
-model = rf(10, verbose=1)
+model = rf(30)
 
 model.fit(x_train, y_train)
 
@@ -28,5 +66,5 @@ y_test = model.predict(x_test)
 y_test = pa.DataFrame(y_test)
 y_test.index = tabtest['datetime']
 
-print(y_test)
+y_test.to_csv('csv/rf_matthias_1.csv')
 
