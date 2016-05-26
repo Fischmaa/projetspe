3391c87d6fd015df0f8efd0ee0a028278a43cd74
Valerian Baillet <valerian.baillet@ensimag.grenoble-inp.fr>
2016-05-26 09:57:53 +0200

    test d'accélération grace au thread

--- expedia/script/data_analysis_thread.py
+++ expedia/script/data_analysis_thread.py
@@ -26,7 +26,11 @@ from threading import Thread, RLock
 verrou = RLock()
 
 start_time = time.time()
-train = pd.read_csv('../train.csv', iterator = True, chunksize = 100000)
+train1 = pd.read_csv('first_data.csv', iterator = True, chunksize = 100000)
+train2 = pd.read_csv('second_data.csv', iterator = True, chunksize = 100000)
+train3 = pd.read_csv('third_data.csv', iterator = True, chunksize = 100000)
+
+
 count_user = 0 
 count_distance = 0
 
@@ -39,14 +43,33 @@ class Analyse (Thread):
 	def run(self):
 		global count_distance
 		global count_user
+		count_local = 0
+		count_dis = 0
 		for chunk in self.train:
 			tab = chunk['orig_destination_distance']
 			for j in range(len(tab)) : 
-				if not isnan(tab[j]):
-					with verrou:
-						count_user += 1
-						count_distance += tab[j]
+				if not mt.isnan(tab[j]) :
+					count_local += 1
+					count_dis += tab[j]
+		with verrou:
+			count_user += count_local
+			count_distance += count_dis
+		print('fin')
 
 
 
+thread_1 = Analyse(train1)
+thread_2 = Analyse(train2)
+thread_3 = Analyse(train3)
+
+thread_1.start()
+thread_2.start()
+thread_3.start()
+
+thread_1.join()
+thread_2.join()
+thread_3.join()
+
+
+print (count_user/count_distance)
 print("--- %s seconds ---" % (time.time() - start_time))
