89976ef7ef130ec021f8c33cffd2ec941411f955
Valerian Baillet <valerian.baillet@ensimag.grenoble-inp.fr>
2016-06-01 14:20:48 +0200

    Ajout du script bourrin corrigé

--- expedia/script/xgboost_matthias.py
+++ expedia/script/xgboost_matthias.py
@@ -20,55 +20,68 @@
 import xgboost as xgb
 import pandas as pd
 import numpy as np
-
+import os
 
 # In[4]:
 
 # Lecture du csv avec Pandas
 
 train = pd.read_csv('../train.csv',iterator = True, chunksize = 1000000)
-test = pd.read_csv('../test.csv' , iterator = True, chunksize = 1000000)
-
 tab_train = train.get_chunk()
-tab_test = test.get_chunk()
 print('creation premier tableau...')
 for chunk in train :
 	tab_train = pd.concat([tab_train, chunk])
-
-print('creation second tableau...')
-for chunk in test :
-	tab_test = pd.concat([tab_test, chunk])
-
-# In[8]:
+print('fin premier tableau')
 
 # On prépare les données pour l'algorithme
 print ('début pretraitement...')
 X_train = tab_train.drop(['hotel_cluster','date_time','srch_ci','srch_co',],1).as_matrix()
 Y_train = tab_train['hotel_cluster'].as_matrix()
+print ('fin pretraitement')
+
+#creation du modèle avec fitting
+print('début modèle...')
+gbm = xgb.XGBClassifier(max_depth=4, n_estimators=300, learning_rate=0.05)
+gbm.fit(X_train,Y_train)
+print('fin modèle...')
+
+#sauvegarde modèle
+print('sauvegarde du modèle...')
+if not os.path.isdir('modeles'):
+	os.mkdir('modeles')
+gbm.booster().save_model('modeles/premier.model')
+print('fin sauvegarde...')
+
+#sauvegarde feature
+print('sauvegarde feature...')
+if not os.path.isdir('features'):
+	os.mkdir('features')
+feature = pd.Series(gbm.booster().get_fscore()).sort_values(ascending = False)
+feature.to_csv('features/premier_feature.csv')
+print('fin sauvegarde...')
+
+#on utilise le tableau test
+test = pd.read_csv('../test.csv' , iterator = True, chunksize = 1000000)
+tab_test = test.get_chunk()
+print('creation second tableau...')
+for chunk in test :
+	tab_test = pd.concat([tab_test, chunk])
+print('fin second tableau...')
 
 X_test = tab_test.drop(['date_time','srch_ci','srch_co'],1).as_matrix()
+
+#prédiction pour le test
+print('début prédiction...')
+Y_test = gbm.predict(X_test)
+print('fin prédiction...')
+
+#on recupère le résultat
 result = tab_test[['id']]
 result = result.drop(['id'],1)
-
-print ('fin pretraitement')
-#type(result)
-
-
-# In[ ]:
-
-gbm = xgb.XGBClassifier(max_depth=4, n_estimators=300, learning_rate=0.05)
-
-gbm.fit(X_train,Y_train)
-
-Y_test = gbm.predict(X_test)
-
-
-# In[ ]:
-
 result["hotel_cluster"]=pd.DataFrame(Y_test)
-result.to_csv('out_xgboost.csv')
-#features = [ "your list of features ..." ]
-#mapFeat = dict(zip(["f"+str(i) for i in range(len(features))],features))
-#ts = pd.Series(gbm.booster().get_fscore())
-#ts.index = ts.reset_index()['index'].map(mapFeat)
-#ts.order()[-15:].plot(kind="barh", title=("features importance"))
+
+print('début exportation...')
+if not os.path.isdir('resultat'):
+	os.mkdir('resultat')
+
+result.to_csv('resultat/out_xgboost.csv')
